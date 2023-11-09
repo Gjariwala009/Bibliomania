e69f5c6610612806e0ac65839fdc34069e15d7b4
gjjariwala <gv554747@dal.ca>
2023-11-09 14:09:41 -0400

    lab 7 completed

---
+++ labs/lab7/connection.php
@@ -0,0 +1,20 @@
+<?php
+
+$username = "gjjariwala";
+$server = "db.cs.dal.ca";
+$password = "9p9HTgWS5K9HAJWn36wJpB2Ai";
+
+try {
+
+    $pdo = new PDO("mysql:host=db.cs.dal.ca;dbname=amp", $username, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec('SET NAMES "utf8"');
+} catch (Exception $e) {
+
+    echo 'Unable to connect to the database server.' . $e->getMessage();
+
+    exit();
+}
+
+echo 'Database connection established';
