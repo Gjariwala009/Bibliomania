5c32cda26b32c4ce31ecc6d2228186ed8373fb8c
gjjariwala <gv554747@dal.ca>
2023-11-21 11:31:36 -0400

    Lab 3 completed with regex and file distribution

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -23,90 +23,30 @@ include 'logic.php';
                     <div class="form-group" id="first_name_field">
                         <label for="first_name"><b>First Name</b></label>
                         <input type="text" class="form-control" placeholder="Enter First Name" name="firstName" id="first_name">
-                        <?php
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            if (isset($_POST["submit"])) {
-                                if (!empty($firstName) && !filter_var($firstName, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z]+( [a-zA-Z]+)*$/")))) {
-                                    echo $errorFirstName;
-                                    $isValid = false;
-                                }
-                            }
-                        }
-                        ?>
                     </div>
 
                     <div class="form-group" id="last_name_field">
                         <label for="last_name"><b>Last Name</b></label>
                         <input type="text" class="form-control" placeholder="Enter Last Name" name="lastName" id="last_name">
-                        <?php
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            if (isset($_POST["submit"])) {
-                                if (!empty($lastName) && !filter_var($lastName, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-z'-]+$/")))) {
-                                    echo $errorLastName;
-                                    $isValid = false;
-                                }
-                            }
-                        }
-                        ?>
                     </div>
 
                     <div class="form-group">
                         <label for="email_address"><b>Email Address</b></label>
                         <input type="text" class="form-control" placeholder="Enter Email Address" name="emailAddress">
-                        <?php
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            if (isset($_POST["submit"])) {
-                                if (!empty($emailAddress) && !filter_var($emailAddress, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/')))) {
-                                    echo $errorEmail;
-                                    $isValid = false;
-                                }
-                            }
-                        }
-                        ?>
                     </div>
 
                     <div class="form-group" id="password_field">
                         <label for="password"><b>Password</b></label>
                         <input type="password" class="form-control" placeholder="Enter Password" name="password" id="password">
-                        <?php
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            if (isset($_POST["submit"])) {
-                                if (!filter_var($password, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{12,}$/")))) {
-                                    echo $errorPassword;
-                                    $isValid = false;
-                                }
-                            }
-                        }
-                        ?>
                     </div>
 
                     <div class="form-group" id="confirm_password_field">
                         <label for="confirm_password"><b>Confirm Password</b></label>
                         <input type="password" class="form-control" placeholder="Confirm Password" name="confirmPassword" id="confirm_password">
-                        <?php
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            if (isset($_POST["submit"])) {
-                                if ($password != $confirmPassword) {
-                                    echo $errorConfirmPassword;
-                                    $isValid = false;
-                                }
-                            }
-                        }
-                        ?>
                     </div>
 
                     <div class="form-group" id="button_field">
                         <button type="submit" class="btn btn-primary" name="submit">Submit</button>
-                        <?php
-                        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                            if (isset($_POST["submit"])) {
-                                if ($isValid) {
-                                    header("Location: welcome.php");
-                                    exit;
-                                }
-                            }
-                        }
-                        ?>
                         <button type="reset" class="btn btn-secondary" name="Reset">Reset</button>
                     </div>
                 </div>
--- labs/lab3/logic.php
+++ labs/lab3/logic.php
@@ -17,3 +17,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $errorConfirmPassword = "<p class=\"text-alert\">Passwords do not match! Try again.</p>";
     }
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["submit"])) {
+        if (!empty($firstName) && !filter_var($firstName, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z]+( [a-zA-Z]+)*$/")))) {
+            echo $errorFirstName;
+            $isValid = false;
+        }
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["submit"])) {
+        if (!empty($lastName) && !filter_var($lastName, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-z'-]+$/")))) {
+            echo $errorLastName;
+            $isValid = false;
+        }
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["submit"])) {
+        if (!empty($emailAddress) && !filter_var($emailAddress, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/')))) {
+            echo $errorEmail;
+            $isValid = false;
+        }
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["submit"])) {
+        if (!filter_var($password, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{12,}$/")))) {
+            echo $errorPassword;
+            $isValid = false;
+        }
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["submit"])) {
+        if ($password != $confirmPassword) {
+            echo $errorConfirmPassword;
+            $isValid = false;
+        }
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["submit"])) {
+        if ($isValid) {
+            header("Location: welcome.php");
+            exit;
+        }
+    }
+}
